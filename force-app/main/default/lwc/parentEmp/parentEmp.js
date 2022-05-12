86113c79a9846ff35bfbc2ede2077ff5d2ca329a
monalijagtap8 <monali.jagtap@ceptes.com>
2022-05-12 11:45:36 +0530

    Added comment

--- force-app/main/default/lwc/parentEmp/parentEmp.js
+++ force-app/main/default/lwc/parentEmp/parentEmp.js
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
-
+//Comment
 export default class ParentEmp extends LightningElement {
     updateEmp(){
         this.template.querySelector('c-employee').updateEmp();
